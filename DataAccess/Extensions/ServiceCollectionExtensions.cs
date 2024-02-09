99686dc54a0ac16f2cf863224f50c772678aea63
DaniilKarpushko <steamovsky525@mail.ru>
2024-02-09 15:51:49 +0300

    feat: added DataAccess Layer

---
+++ DataAccess/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Application.Abstractions.Repositories;
+using DataAccess.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructureDataAccess(
+        this IServiceCollection collection)
+    {
+        collection.AddScoped<IAlgorithmRepository, AlgorithmRepository>();
+
+        return collection;
+    }
+}
---
+++ DataAccess/Repository/AlgorithmRepository.cs
@@ -0,0 +1,33 @@
+﻿using Application.Abstractions.Repositories;
+using Application.Models;
+using System.Text.Json;
+
+
+namespace DataAccess.Repository;
+
+public class AlgorithmRepository : IAlgorithmRepository
+{
+    private string _jsonLib;
+    private string _jsonList;
+
+    public AlgorithmRepository(AppConfiguration appConfiguration)
+    {
+        _jsonLib = appConfiguration.AlgorithmDataFile;
+        _jsonList = appConfiguration.AlgorithmListFile;
+    }
+
+    public async Task<Algorithm?> GetAlgorithm(string name)
+    {
+        var txt = new StreamReader(_jsonLib);
+        var res = await JsonSerializer.DeserializeAsync<List<Algorithm>>(txt.BaseStream);
+        
+        return res.Find(arg => arg.Name == name);
+    }
+
+    public async Task<List<string>?> GetAccessibleAlgorithms()
+    {
+        var txt =  new StreamReader(_jsonList);
+        var list = await JsonSerializer.DeserializeAsync<List<string>>(txt.BaseStream);
+        return list;
+    }
+}
